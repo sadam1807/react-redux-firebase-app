87e665490050b690579a5c4c0a7869aa2d67856d
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-15 20:29:51 +0530

    middleware using thunk

--- project-managment/src/store/reducers/projectReducer.js
+++ project-managment/src/store/reducers/projectReducer.js
@@ -3,11 +3,19 @@ const initState = {
         {id: '1', title: 'help me find peach', content: 'blah blah blah'},
         {id: '2', title: 'collect all the stars', content: 'blah blah blah'},
         {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah'}
-    ]
+    ],
+    newData: ''
 }
 
 const projectReducer = (state = initState, action ) => {
-    return state
+   switch (action.type) {
+       case 'CREATE_PROJECT':
+         console.log('created project', action.project)
+         return state
+        default:
+        return state;
+   } 
+    //return state
 }
 
 export default projectReducer;
