95114cb406466001f1500d04014db254f22c1da5
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-16 16:22:39 +0530

    adding data to firebase

--- project-managment/src/store/actions/projectActions.js
+++ project-managment/src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) =>{
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore }) => {
         //make asyn call
-        dispatch({ type: 'CREATE_PROJECT', project });
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+           ...project,
+           authorFirst: 'Net',
+           authorLastname: 'Ninja',
+           authorId: 12345,
+           createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
     }
 }
