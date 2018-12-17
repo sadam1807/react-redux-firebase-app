91c4b5b86ec9fd75828e175e97ab325b9d63972a
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-18 01:09:18 +0530

    sigin and signout from firebase

--- project-managment/src/store/actions/authActions.js
+++ project-managment/src/store/actions/authActions.js
@@ -11,3 +11,13 @@ export const signIn = (credentials) => {
        })
    }
 }
+
+
+export const signOut = (credentials) => {
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase()
+        firebase.auth().signOut().then(() => {
+            dispatch({ type: 'LOGOUT_SUCSSES' })
+        })
+    }
+ }
--- project-managment/src/store/reducers/authReducer.js
+++ project-managment/src/store/reducers/authReducer.js
@@ -1,7 +1,33 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action ) => {
-    return state
+     switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log('login error');
+            return {
+                ...state,
+                authError: 'login failed'
+            }
+
+        case 'LOGIN_SUCSSES':
+            console.log('login success');
+            return {
+                ...state,
+                authError:null
+            }
+         
+        case 'LOGOUT_SUCSSES':
+            console.log('logout success');
+            return {
+                ...state,
+                authError:null
+            }    
+
+        default:
+         return state;   
+     }
 }
 
 export default authReducer;
