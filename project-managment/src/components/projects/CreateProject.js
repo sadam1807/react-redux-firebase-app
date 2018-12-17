c6c6ad96c6ba15b6e2e82b26f837f7d7b15679ec
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-18 02:27:29 +0530

    redirect with auth status

--- project-managment/src/components/projects/CreateProject.js
+++ project-managment/src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { createProject} from '../../store/actions/projectActions'
+import { Redirect } from 'react-router-dom'
 
 class CreateProject extends Component {
   state = {
@@ -20,6 +21,8 @@ class CreateProject extends Component {
 }
 
   render() {
+    const { auth } = this.props;
+    if(!auth.uid) return <Redirect to='/signin/' />
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -41,7 +44,11 @@ class CreateProject extends Component {
   }
 }
 
-
+const mapStateToProps = (state) => {
+    return{
+        auth: state.firebase.auth
+    }
+}
 
 const mapDispatchToProps = (dispatch) => {
      return{
@@ -49,4 +56,4 @@ const mapDispatchToProps = (dispatch) => {
      }
 }
 
-export default connect(null ,mapDispatchToProps)(CreateProject)
+export default connect(mapStateToProps ,mapDispatchToProps)(CreateProject)
